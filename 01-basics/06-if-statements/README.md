00fca9ae07f12abd7838a7b6b9320e8defcb47d0
Roland Salloum <roland.salloum00@outlook.com>
2022-09-24 16:53:47 +0300

    feat: conditional statements (if-else, ternary, match)

---
+++ 01-basics/06-if-statements/README.md
@@ -0,0 +1,37 @@
+# Conditional statements
+
+## if-statements 
+if-statements in rust is very similar to if-statements in java or c#. 
+
+```rust
+    if (condition) {
+        // logic
+    } else if(condition1 && condition2)  {
+         // logic
+    } else {
+        // logic
+    }
+```
+
+## ternary operation
+
+```rust
+if condition {
+        // logic
+    } else {
+        // logic
+    };
+```
+
+## Match
+
+``` rust
+  
+    match condition {
+        // specify condition
+        _ => {} // default condition
+    }
+    
+```
+
+Check the Code!
---
+++ 01-basics/06-if-statements/src/main.rs
@@ -0,0 +1,40 @@
+use std::cmp::Ordering;
+
+fn main() {
+    let car_model:i32 = 2000;
+    if (car_model > 2018) {
+        println!("New Model \n")
+    } else if(car_model >= 2000 && car_model < 2018)  {
+        print!("Still Almost new \n");
+    } else {
+        println!("Old car \n")
+    }
+
+    // ternary operator
+
+    let my_age = 21;
+    let can_vote = if my_age >= 18{
+        true
+    } else {
+        false
+    };
+    println!("Can vote: {}", can_vote);
+
+    // Match
+
+    let car_model = 1998;
+    match car_model {
+        2017..= 2020 => println!("New Model"),
+        2000 => println!("The 2000s Models!"),
+        i32::MIN ..=1999 => println!("Oldies models"),
+        _ => {}
+    }
+
+    // Match example 2
+    let license_age = 18;
+    match my_age.cmp(&license_age) {
+        Ordering::Less => println!("Cannot Drive"),
+        Ordering::Greater | Ordering::Equal => println!("Can Drive"),
+        _ => {}
+    }
+}
